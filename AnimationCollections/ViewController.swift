ac4fb3499dabef90cfce023e29f40560ef3e7bc5
mr.scorpion <mrscorpion1114@gmail.com>
2016-08-03 10:22:51 +0800

    add refresh

--- AnimationCollections/ViewController.swift
+++ AnimationCollections/ViewController.swift
@@ -31,6 +31,9 @@ class ViewController: UITableViewController {
         Entry(title: "The Unknown", image: UIImage(named: "six")!)
     ]
     
+//    // 定义全局对象变量
+//    var cusRefreshControl: ZJRefreshControl!
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +42,18 @@ class ViewController: UITableViewController {
         configureNavigationController()
         
         self.view.backgroundColor = UIColor.whiteColor()
+        
+//        // 初始化
+//        //只有下拉刷新
+//        cusRefreshControl = ZJRefreshControl(scrollView: self.tableView, refreshBlock: {
+//            self.dropViewDidBeginRefreshing()
+//        })
+//        //下拉刷新和上拉加载更多
+//        cusRefreshControl = ZJRefreshControl(scrollView: self.tableView,refreshBlock: {
+//            self.dropViewDidBeginRefreshing();
+//            },loadmoreBlock: {
+//                self.dropViewDidBeginLoadmore();
+//        });
     }
 
     func configureNavigationController() {
@@ -60,6 +75,33 @@ class ViewController: UITableViewController {
     }
     
     
+//    // MARK: - Refresh
+//    //下拉刷新调用的方法
+//    func dropViewDidBeginRefreshing()->Void{
+//        print("-----刷新数据-----");
+//        self.delay(1.5, closure: {
+//            //结束下拉刷新必须调用
+//            self.cusRefreshControl.endRefreshing();
+//        });
+//    }
+//    //上拉加载更多调用的方法
+//    func dropViewDidBeginLoadmore()->Void{
+//        print("-----加载数据-----");
+//        self.delay(1.5, closure: {
+//            //结束加载更多必须调用
+//            self.cusRefreshControl.endLoadingmore();
+//        });
+//    }
+//    //延迟执行方法
+//    func delay(delay:Double, closure:()->()) {
+//        dispatch_after(
+//            dispatch_time(
+//                DISPATCH_TIME_NOW,
+//                Int64(delay * Double(NSEC_PER_SEC))
+//            ),
+//            dispatch_get_main_queue(), closure)
+//    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
